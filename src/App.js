f308c0a0fff8d4de8042c7303c63af8b3e3b9617
Dario Spina <dariospina@Darios-MBP.broadband>
2023-06-28 08:26:20 +0100

    sky movement

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import bgImage from './background-image.jpg';  // adjust the path as needed
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [myData, setMyData] = useState([]);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [movement, setMovement] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    fetch('https://sheetdb.io/api/v1/5uxyugigju92s')
-      .then(response => response.json())
-      .then(data => setData(data));
+    const fetchData = async () => {
+      const result = await axios(
+        'https://sheetdb.io/api/v1/5uxyugigju92s',
+      );
+      setMyData(result.data);
+    };
+
+    fetchData();
   }, []);
 
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({
+        x: event.clientX - window.innerWidth / 2,
+        y: event.clientY - window.innerHeight / 2,
+      });
+    }
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    }
+  }, []);
+
+  useEffect(() => {
+    const moveBackground = () => {
+      const speedFactor = 0.01;  // adjust as needed
+      setMovement(prev => ({
+        x: prev.x - speedFactor * position.x,
+        y: prev.y - speedFactor * position.y,
+      }));
+    }
+
+    const interval = setInterval(moveBackground, 10);  // adjust as needed
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [position]);
+
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      {data.map((row, index) => (
+    <div className="App" style={{
+      backgroundImage: `url(${bgImage})`,
+      backgroundPosition: `${movement.x}px ${movement.y}px`,
+    }}>
+      {myData.map((data, index) => (
         <div key={index}>
-          <p>{row.Column1}</p>
-          <p>{row.Column2}</p>
+          <h1>{data.Column1}</h1>
+          <h2>{data.Column2}</h2>
         </div>
       ))}
     </div>
